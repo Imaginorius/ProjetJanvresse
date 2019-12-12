bcd48a20808211cf47cd68ffc08484ada99bec4f
JeanOryon <48481925+JeanOryon@users.noreply.github.com>
2019-12-12 22:24:10 +0100

    update change folder
    update

---
+++ JS/style.js
@@ -0,0 +1,29 @@
+
+width = true;
+
+function openNav() {
+  var lscreen=screen.width;
+    if (width){
+    document.getElementById("menu").style.width= "50%";
+    document.getElementById("menu").style.opacity="0.9";
+    document.body.style.backgroundColor="rgba(0,0,0,0.4)";
+    /*
+    document.getElementById("main").style.marginLeft="51%";
+    */
+    }
+    else {
+      document.getElementById("menu").style.width="0";
+      document.body.style.backgroundColor="white";
+
+      /*
+      document.getElementById("main").style.marginLeft="0";
+      */
+    }
+    width=!width;
+}
+
+function closeNav() {
+    document.getElementById("menu").style.width= "0";
+    document.getElementById("main").style.marginLeft="0";
+}
+
